b156c8e9ebc2842fa33f371630e7e21ac72a8906
jromana-dev <javier.romana.dev@gmail.com>
2026-08-30 11:38:06 +0200

    chore: set up Maven project with Spring Boot, JPA, H2 and test starter

---
+++ prices-service/src/main/java/com/example/prices_service/PricesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.prices_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesServiceApplication.class, args);
+	}
+
+}
